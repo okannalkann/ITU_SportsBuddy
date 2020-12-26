2aa0db8e046c6589036ac7ab43d3a9718dd7b26c
Okan Alkan <okann.alkann@hotmail.com>
2020-12-26 16:05:31 +0300

    reset password updated

--- server.py
+++ server.py
@@ -14,10 +14,15 @@ app.secret_key = "SportBuddy"
 db = Database("127.0.0.1", 3306, "root", "qwerty123456", "mydb")
 db.con.cursor()    
 
+query = "SELECT * FROM mydb.website_mail"
+db.cursor.execute(query)
+site_mail = db.cursor.fetchone()
+print("site mail",site_mail[1], "site", site_mail[2])
+
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'no.reply.sportsbuddy@gmail.com'
-app.config['MAIL_PASSWORD'] = 'Alkano16'
+app.config['MAIL_USERNAME'] = str(site_mail[1])
+app.config['MAIL_PASSWORD'] = str(site_mail[2])
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
